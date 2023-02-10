1033fac980c86ee921910e87f4a19967c334b814
Christian Gossage <gossage.christian@gmail.com>
2023-02-10 12:47:14 -0600

    WorkingOnSearch

--- LiftOff-Project/Controllers/HomeController.cs
+++ LiftOff-Project/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using LiftOff_Project.Migrations;
+﻿using LiftOff_Project.Data;
+using LiftOff_Project.Migrations;
 using LiftOff_Project.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -8,30 +9,43 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Routing;
 
 namespace LiftOff_Project.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         [HttpGet]
-        public IActionResult Index()
+        public async Task<IActionResult> Index(string searchString)
         {
-            return View();
+            var movies = from m in _context.Songs
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            return View(await movies.ToListAsync());
         }
-        
+
         [HttpGet]
         [Route("/Home/Search")]
         public IActionResult Search()
         {
             return View();
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
