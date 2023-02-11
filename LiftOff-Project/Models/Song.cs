ddd3f0b2bdb86b3f92814ce9b7f4d9c6cef08c5e
Christian Gossage <gossage.christian@gmail.com>
2023-02-10 18:49:59 -0600

    ResetingMigrations

--- LiftOff-Project/Models/Song.cs
+++ LiftOff-Project/Models/Song.cs
@@ -2,18 +2,18 @@
 {
     public class Song
     {
+        public int SongId { get; set; }
         public string Artist { get; set; }
         public string Title { get; set; }
-        public string Link { get; set; }
-        public string Lyrics { get; set; }
-        public int SongId { get; set; }
+        public string Genre { get; set; }
+        public string Year { get; set; }
 
-        public Song(string artist, string title, string link, string lyrics)
+        public Song(string artist, string title, string genre, string year)
         {
             Artist = artist;
             Title = title;
-            Link = link;
-            Lyrics = lyrics;
+            Genre = genre;
+            Year = year;
         }
         public Song()
         {
